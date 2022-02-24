b9222e160f4f95ae1d0762b0117e655b211d56ad
AnandSinghYadav <anandyadav2332@gmail.com>
2022-02-24 09:39:30 +0530

    initiated by anand just once

---
+++ jsFiles/brandNewCar.js
@@ -0,0 +1,85 @@
+
+// js for swaping
+
+function newcar()
+{
+    
+    var brandNew ="brandNewCar.html";
+    window.location.href=brandNew;
+    
+}
+function unBox()
+{
+    
+    var brandNew ="hunMahiPage.html";
+    window.location.href=brandNew;
+    
+
+}
+
+// for showing car data 
+
+var car = JSON.parse(localStorage.getItem("unBoxCars"));
+
+console.log(car);
+function showcar(car)
+{
+    car.map(function(ele)
+    {
+        var mainDiv = document.createElement("div");
+         
+        var carImage = document.createElement("img");
+        carImage.setAttribute("src",ele.image);
+        carImage.setAttribute("id","carImage");
+
+        var subDiv1 = document.createElement("div");
+        subDiv1.setAttribute("id","carInfo");
+
+        var carName=document.createElement("h1");
+        carName.innerText=ele.name;
+        var carType = document.createElement("p");
+        carType.innerText=ele.type;
+        carType.setAttribute("class","carType");
+        
+        var typeDiv = document.createElement("div");
+        typeDiv.setAttribute("id","typeDiv");
+
+        var logo1 = document.createElement("img");
+        logo1.setAttribute("src","https://www.revv.co.in/grapheneImages/CarsAndPricing/transmission-icon.svg");
+        logo1.setAttribute("class","logo");
+        var fuel_typo = document.createElement("p");
+        fuel_typo.innerText=ele.fuel_type;
+        fuel_typo.setAttribute("id","fuel");
+
+        var logo2 = document.createElement("img");
+        logo2.setAttribute("src","https://www.revv.co.in/grapheneImages/CarsAndPricing/automatic-icon.svg");
+        logo2.setAttribute("class","logo");
+        var carFuntion = document.createElement("p");
+        carFuntion.innerText=ele.function;
+        carFuntion.setAttribute("id","function");
+
+        typeDiv.append(logo1,fuel_typo,logo2,carFuntion);
+
+        subDiv1.append(carName,carType,typeDiv);
+
+        var priceDiv = document.createElement("div");
+
+        var perMonPrice = document.createElement("h2");
+         perMonPrice.innerHTML="₹"+ele.perMonthPrice+"/month";
+
+         var originalP = document.createElement("h1");
+         originalP.innerText="₹"+ele.originalPrice;
+         originalP.style.textDecoration="line-through";
+         originalP.style.color="#808080";
+
+         priceDiv.append(perMonPrice,originalP);
+
+         mainDiv.append(carImage,subDiv1,priceDiv);
+
+         document.getElementById("Cars").append(mainDiv);
+
+
+    });
+}
+
+showcar(car);
---
+++ jsFiles/cars.js
@@ -0,0 +1,140 @@
+var cars = JSON.parse(localStorage.getItem("carDataBase"));
+
+  console.log(cars[2]);   
+console.log(cars);
+
+function displayCar(cars)
+{
+      document.getElementById("container").innerHTML="";
+      cars.map(function (ele) {
+       
+        var mainDiv = document.createElement("div");
+
+        var name=document.createElement("h4");
+        name.innerText=ele.name;
+
+        var img = document.createElement("img");
+         img.setAttribute("src",ele.Image);
+         img.setAttribute("id","carImage");
+
+        
+  
+
+       var typeMainDiv = document.createElement("div");
+
+       typeMainDiv.setAttribute("id","typeMain");
+
+
+       var typeSubDiv1 = document.createElement("div");       //  subdiv1
+
+       var logo1 = document.createElement("img");
+       logo1.setAttribute("src","https://www.revv.co.in/imgs/car-card/automatic.svg");
+
+       var type = document.createElement("p");
+       type.innerText=ele.function;
+
+       typeSubDiv1.append(logo1,type);
+
+       var typeSubDiv2 = document.createElement("div");           // subdiv2
+
+       var logo2 = document.createElement("img");
+       logo2.setAttribute("src","https://www.revv.co.in/imgs/car-card/petrol.svg");
+       var fuel_type = document.createElement("p");
+       fuel_type.innerText=ele.fuel_type;
+
+       typeSubDiv2.append(logo2,fuel_type);
+
+       var typeSubDiv3 = document.createElement("div");                 //subdiv3
+
+       var logo3 = document.createElement("img");
+       logo3.setAttribute("src","https://www.revv.co.in/imgs/car-card/seat.svg");
+       var seat = document.createElement("p");
+       seat.innerText=ele.seat+" "+"Seats";
+
+       typeSubDiv3.append(logo3,seat);
+
+       typeMainDiv.append(typeSubDiv1,typeSubDiv2,typeSubDiv3);
+
+     
+
+       priceTypeMainDiv = document.createElement("div");
+       priceTypeMainDiv.setAttribute("id","PriceMain");
+
+
+        var priceSubDiv1 = document.createElement("div");
+        priceSubDiv1.setAttribute("class","priceRange");
+
+      
+        var price_270_Kms = document.createElement("p");
+        price_270_Kms.innerText="₹"+" "+ele.kms_270;
+
+        
+
+         var Kms_range1 = document.createElement("p")
+         Kms_range1.setAttribute("id","kmsPTag");
+         Kms_range1.innerText="270Kms";
+         
+         priceSubDiv1.append(price_270_Kms,Kms_range1);
+
+         
+         var priceSubDiv2 = document.createElement("div");
+         priceSubDiv2.setAttribute("class","priceRange");
+         
+
+      
+         var price_450_Kms = document.createElement("p");
+         price_450_Kms.innerText="₹"+" "+ele.kms_450;
+ 
+       
+ 
+          var Kms_range2 = document.createElement("p")
+          Kms_range2.setAttribute("id","kmsPTag");
+          Kms_range2.innerText="450Kms";
+          
+          priceSubDiv2.append(price_450_Kms,Kms_range2);
+
+
+          var priceSubDiv3 = document.createElement("div");
+          priceSubDiv3.setAttribute("class","priceRange");
+
+        
+          var price_675_Kms = document.createElement("p");
+          price_675_Kms.innerText="₹"+" "+ele.kms_675;
+  
+        
+  
+           var Kms_range3 = document.createElement("p")
+           Kms_range3.setAttribute("id","kmsPTag");
+           Kms_range3.innerText="675Kms";
+           
+           priceSubDiv3.append(price_675_Kms,Kms_range3);
+
+           // appending all different range in mainprice div
+
+           priceTypeMainDiv.append(priceSubDiv1,priceSubDiv2,priceSubDiv3);
+
+          var extraChargeDiv = document.createElement("div");
+          extraChargeDiv.setAttribute("class","charge");
+
+          var extraCharge = document.createElement("p");
+          extraCharge.innerText="Extra km charge @ ₹"+ele.exChargePerKms +"/km";
+
+          var availButton = document.createElement("button");
+          availButton.setAttribute("id","bookButton");
+          availButton.textContent=ele.available;
+   
+          extraChargeDiv.append(extraCharge,availButton);
+          
+
+
+
+        mainDiv.append(name,img,typeMainDiv,priceTypeMainDiv,extraChargeDiv);
+
+
+        document.getElementById("container").append(mainDiv);
+           
+    });
+ 
+}
+
+displayCar(cars);
