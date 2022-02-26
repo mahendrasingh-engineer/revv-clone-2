4647d2f8883193d723f041c021cf28a1370e8053
AnandSinghYadav <anandyadav2332@gmail.com>
2022-02-27 00:07:36 +0530

    connectivity by anand

--- jsFiles/brandNewCar.js
+++ jsFiles/brandNewCar.js
@@ -4,6 +4,7 @@
 function newcar()
 {
     
+    
     var brandNew ="brandNewCar.html";
     window.location.href=brandNew;
     
@@ -19,28 +20,30 @@ function unBox()
 
 // for showing car data 
 
-var car = JSON.parse(localStorage.getItem("unBoxCars"));
-
+var car = JSON.parse(localStorage.getItem("hyundaiElite"));
 console.log(car);
+var count=1;
+
 function showcar(car)
 {
     car.map(function(ele)
     {
         var mainDiv = document.createElement("div");
-         
-        var carImage = document.createElement("img");
-        carImage.setAttribute("src",ele.image);
-        carImage.setAttribute("id","carImage");
-
-        var subDiv1 = document.createElement("div");
-        subDiv1.setAttribute("id","carInfo");
-
-        var carName=document.createElement("h1");
+         mainDiv.setAttribute("id","child"+count++)
+         mainDiv.addEventListener("click",function()
+         {
+             addCar(ele);
+         });
+        var carName=document.createElement("p");
         carName.innerText=ele.name;
-        var carType = document.createElement("p");
-        carType.innerText=ele.type;
-        carType.setAttribute("class","carType");
-        
+        carName.setAttribute("id","carName")
+
+        var carVersion=document.createElement("b");
+        carVersion.innerText=ele.version;
+        carVersion.setAttribute("id","version")
+
+
+    
         var typeDiv = document.createElement("div");
         typeDiv.setAttribute("id","typeDiv");
 
@@ -60,26 +63,147 @@ function showcar(car)
 
         typeDiv.append(logo1,fuel_typo,logo2,carFuntion);
 
-        subDiv1.append(carName,carType,typeDiv);
 
         var priceDiv = document.createElement("div");
 
-        var perMonPrice = document.createElement("h2");
-         perMonPrice.innerHTML="₹"+ele.perMonthPrice+"/month";
+        var perMonPrice = document.createElement("h1");
+         perMonPrice.innerHTML="₹"+ele.perMonthPrice+"<sub style='fontWeight:100; color:#808080; font-size:20px;'>/month</sub>";
 
-         var originalP = document.createElement("h1");
+         var originalP = document.createElement("h2");
          originalP.innerText="₹"+ele.originalPrice;
          originalP.style.textDecoration="line-through";
          originalP.style.color="#808080";
-
+        
          priceDiv.append(perMonPrice,originalP);
 
-         mainDiv.append(carImage,subDiv1,priceDiv);
+         mainDiv.append(carName,carVersion,typeDiv,priceDiv);
 
-         document.getElementById("Cars").append(mainDiv);
+         document.getElementById("varient").append(mainDiv);
 
 
     });
 }
 
 showcar(car);
+
+
+
+function byRange()
+{
+    var stRange = document.getElementById("startRange").value;
+    var endRange = document.getElementById("endRange").value;
+    
+    var result = car.filter(function(ele)
+    {
+        return ele.perMonthPrice >=Number(stRange) && ele.perMonthPrice<=Number(endRange);
+    })
+
+    showcar(result);
+    console.log(result);
+}
+
+
+function searchFun()
+{
+        var text = document.getElementById("search").value;
+        var searchResult = car.filter(function(ele)
+        {
+            return ele.name.includes(text.toUpperCase());
+        })
+
+        showcar(searchResult);
+        console.log(searchResult);
+     
+}
+
+function sortFun()
+{
+    var value = document.getElementById("sorting").value;
+    if(value=='h2l')
+    {
+         car.sort(function(a,b)
+         {
+             return b.perMonthPrice-a.perMonthPrice;
+         })
+         showcar(car);
+    }
+    else if(value=='l2h')
+    {
+        car.sort(function(a,b)
+        {
+            return a.perMonthPrice-b.perMonthPrice;
+        })
+        showcar(car);
+        console.log(car)
+    }
+}
+
+
+
+
+
+var left=1;
+var right=3;
+function show()
+{   
+    for(var i=left;i<=right;i++)
+    {
+    document.getElementById("child"+i).style.display="inline-block";
+    }
+}
+
+
+   function moveLeft()
+    {
+
+      if(left<=3&&right<=5)
+      {
+
+        document.getElementById("child"+left).style.display="none";
+        left+=1;
+        right+=1;
+        for(var i=left;i<=right;i++)
+       {
+         document.getElementById("child"+i).style.display="inline-block";
+       }
+
+    }
+    else
+    {
+        return;
+    }
+}
+
+function moveRight()
+{
+
+  if(left>=3&&right>=4)
+  {
+
+    document.getElementById("child"+right).style.display="none";
+    left-=1;
+    right-=1;
+
+    for(var i=left;i<=right;i++)
+   {
+     document.getElementById("child"+i).style.display="inline-block";
+   }
+
+}
+else
+{
+    return;
+}
+}
+
+
+var unbox = [];
+
+function addCar(ele)
+{
+      unbox.push(ele);
+      localStorage.setItem("unBoxData",JSON.stringify(unbox));
+
+      var get = JSON.parse(localStorage.getItem("unBoxData"));
+     window.location.href="firstPayment.html";
+}
